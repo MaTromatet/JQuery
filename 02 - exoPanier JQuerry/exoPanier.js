d90752127be7204408247bf0529658ce615ab648
MaT <mromatet@gmail.com>
2022-02-21 15:20:56 +0100

    fini exo rideau

--- 02 - exoPanier JQuerry/exoPanier.js
+++ 02 - exoPanier JQuerry/exoPanier.js
@@ -1,21 +1,42 @@
 $(document).ready(function () {
+  $("input[type=button]").prop("disabled", true);
   //Liste Fruit ------------------------------------------------------------------------------
 
-  $("#liste_fruits").change(function () {
+  /*   $("#liste_fruits").change(function () {
     if (!$("#liste_fruits option:selected").length) {
       $("#ajouter").css({ color: "grey", disabled: "false" });
     } else {
       $("#ajouter").css({ color: "black", disabled: "true" });
     }
+  }); */
+
+  $("#liste_fruits").click(function () {
+    $("#ajouter").prop("disabled", false);
   });
 
   //Liste panier ------------------------------------------------------------------------------
 
-  $("#panier").change(function () {
+  /*   $("#panier").change(function () {
     if (!$("#panier option:selected").length) {
       $("#supprimer").css({ color: "grey", disabled: "false" });
     } else {
       $("#supprimer").css({ color: "black", disabled: "true" });
+    }
+  }); */
+
+  $("#panier").click(function () {
+    $("#supprimer").prop("disabled", false);
+  });
+
+  //Click exterieur------------------------------------------------------------------------------
+
+  /*   $("#panier").click(function () {
+    $("#supprimer").css({ color: "black", disabled: "false" });
+  }); */
+
+  $(document).click(function (event) {
+    if (!$(event.target).closest("select").length) {
+      $("input[type=button]").prop("disabled", true);
     }
   });
 
@@ -23,7 +44,7 @@ $(document).ready(function () {
   $("#ajouter").click(function () {
     //Ajoute une nouvelle option dans le select
 
-    $("#liste_fruits option:selected").remove().appendTo("#panier");
+    $("#liste_fruits option:selected").appendTo("#panier");
 
     //Option 1
     // $("#panier").append(new Option(selectedFruit.text(), selectedFruit.val()));
@@ -42,6 +63,6 @@ $(document).ready(function () {
 
   //Bouton Supprimer ------------------------------------------------------------------------------
   $("#supprimer").click(function () {
-    $("#panier option:selected").remove().appendTo("#liste_fruits");
+    $("#panier option:selected").appendTo("#liste_fruits");
   });
 });
